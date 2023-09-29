6cf2336b527bd127713f11d0fa30c5469316fdff
Anna Lukyanenko <anna@shere.me>
2023-09-28 18:56:17 -0700

    created siple to do list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
-
+const todoList = [
+  {
+    id: 1,
+    title: "Complete assignment"
+  },
+  {
+    id: 2,
+    title: "Create branch"
+  },
+  {
+    id: 3,
+    title: "Push to GitHub"
+  },
+  {
+    id: 4,
+    title: "Create pull request"
+  }
+];
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map(function(data){
+          return <li key={data.id}>{data.title}</li>;
+        })}
+      </ul>
     </div>
+
   );
 }
 
